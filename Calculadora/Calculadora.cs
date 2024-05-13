b4effac2ac49e312f8b806862f3a40bfe52c8865
layse lima <layse.lima@confitec.com.br>
2024-05-13 19:28:55 -0300

    primeiro commit

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos;
+
+public class Calculadora
+{ 
+    public static double Somar(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+
+    public static double Subtrair(double n1, double n2)
+    {
+        return n1 - n2;
+    }
+    public static double Multiplicar(double n1, double n2)
+    {
+        return n1 * n2;
+    }
+
+    public static double Dividir(double n1, double n2)
+    {
+        if (n2 != 0)
+        {
+            return n1 / n2;
+        }
+        else
+        {
+            Console.WriteLine("Operação invalida");
+            return 0;
+        }
+    }
+
+    public static double Potencia (double n1, double n2)
+    {
+        return Math.Pow (n1, n2);
+    }
+
+    public static double RaizQuadrada (double n1)
+    {
+        return Math.Sqrt (n1);
+    }
+
+    
+
+
+}
+
+    
+
+
---
+++ Calculadora/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using Calculos;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Digite o símbolo correspondente a operação que deseja realizar:");
+
+        var simbolo = Console.ReadLine();
+        double resultado = 0;
+        if (simbolo == "r")
+        {
+            Console.WriteLine("Digite o número que deseja saber a raiz quadrada:");
+            var n1 = double.Parse(Console.ReadLine());
+             resultado = Calculadora.RaizQuadrada(n1);
+            Console.WriteLine(resultado);
+
+        }
+        else
+        {
+            Console.WriteLine("Digite o primeiro número");
+            var n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            var n2 = double.Parse(Console.ReadLine());
+
+
+            switch (simbolo)
+            {
+                case "+":
+                    resultado = Calculadora.Somar(n1, n2);
+                    break;
+
+                case "-":
+                    resultado = Calculadora.Subtrair(n1, n2);
+                    break;
+
+                case "*":
+                    resultado = Calculadora.Multiplicar(n1, n2);
+                    break;
+
+                case "/":
+                    resultado = Calculadora.Dividir(n1, n2);
+                    break;
+
+                case "**":
+                    resultado = Calculadora.Potencia(n1, n2);
+                    break;
+
+            }
+
+        }
+        Console.WriteLine($"O resultado é {resultado}");
+
+
+    }
+
+}
